ee6135ff91dc7bc2bc9e5f6daa6e1f12899cd3ce
anirudh1492 <anirudhsarthak@gmail.com>
2019-04-20 00:58:00 -0500

    Update app.js

--- airline/app.js
+++ airline/app.js
@@ -26,6 +26,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/public', express.static(__dirname + '/public'));
+
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // catch 404 and forward to error handler
@@ -34,7 +36,7 @@ app.use(function(req, res, next) {
 });
 
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use('/public', express.static(__dirname + '/public'));
+//app.use('/public', express.static(__dirname + '/public'));
 
 app.use(cookieParser());
 app.use(session({secret: "No one  must know ;)"}));
